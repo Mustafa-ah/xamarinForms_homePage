be7c01a647e6e1724898f9358d6fc4b315c95307
mustafa ahmed <mustafa_7070@yahoo.com>
2019-09-27 15:09:13 +0200

    home page first structure

--- MinistryApp/MinistryApp/App.xaml.cs
+++ MinistryApp/MinistryApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace MinistryApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new Views.HomePage();
         }
 
         protected override void OnStart()
